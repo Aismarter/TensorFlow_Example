7c0bc6b19f8d152078c67a6907a1a2000e2104cd
zhangzhiyong <zhangzhiyong@cmsr.chinamobile.com>
2020-05-11 18:06:00 +0800

    使用TFHUB进行文本分类

---
+++ 3.TF_Hub_text_calssfier.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import numpy as np
+import tensorflow as tf
+
+import tensorflow_hub as hub
+import tensorflow_dataset  as tfds
+
+print("version:", tf.__version__)
+print("Eager mode:", tf.executing_eagerly())
+print("Hub version:", hub.__version__)
+print("GPU is","available" if tf.config.experimental.list_physical_devices("CPU") else "NOT AVAILABLE")
